d125a18dae9cb5bbd3c1558ee93b700a7fa09fc5
alvindaiyan <alvindaiyan@gmail.com>
2015-02-11 13:58:07 +1300

    add user user link db table

--- db/userinfo.sql
+++ db/userinfo.sql
@@ -6,3 +6,16 @@ CREATE TABLE userinfo
     Created date,
     CONSTRAINT user_pkey PRIMARY KEY (uid)
 )
+
+CREATE TABLE useruserlink
+(
+    id serial NOT NULL,
+    user1id serial NOT NULL,
+    user2id serial NOT NULL,
+    created date,
+    CONSTRAINT useruserlinkkey PRIMARY KEY (id, user1id, user2id),  
+    FOREIGN KEY (user1id) REFERENCES userinfo(uid),
+    FOREIGN KEY (user2id) REFERENCES userinfo(uid)
+)
+
+ALTER TABLE useruserlink add CONSTRAINT ck_userids check(user1id <> user2id)
